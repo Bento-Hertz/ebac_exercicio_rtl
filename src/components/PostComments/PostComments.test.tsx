f1f2125e029c9a5bc007790ba8054b0c09c479f6
Bento-Hertz <bentoluiz50@gmail.com>
2024-06-02 20:26:50 -0300

    adding test procedure to verify if 2 comments were posted in PostComments

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -3,8 +3,17 @@ import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar 2 comentários na tela', () => {
+        render(<PostComment/>);
+        const botao = screen.getByTestId('btn-add-comentario')
+        fireEvent.click(botao)
+        fireEvent.click(botao)
+    
+        expect(screen.getAllByTestId('post-comment')).toHaveLength(2)
+    })
 });
